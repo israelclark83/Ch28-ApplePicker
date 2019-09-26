c054305bca02276343e5439a0f779f248ba3b162
israelclark83 <54966452+israelclark83@users.noreply.github.com>
2019-09-26 18:30:11 -0400

    Added Scoring and Basket functionality

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
-    public static float bottomY = -20f;                                  // a
+    public static float bottomY = -20f;
 
     void Update()
     {
         if (transform.position.y < bottomY)
         {
-            Destroy(this.gameObject);                                      // b
+            Destroy(this.gameObject);
+
+            // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            // Call the public AppleDestroyed() method of apScript
+            apScript.AppleDestroyed();
         }
     }
 }
